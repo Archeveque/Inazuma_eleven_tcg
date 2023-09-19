6252e8ff4a1a9d79b303516a572330183e6192f6
DamiC0de <dreymidot@gmail.com>
2023-09-19 15:19:52 +0200

    edit routes.rb

--- Inazuma-card-website/src/components/Deckview.jsx
+++ Inazuma-card-website/src/components/Deckview.jsx
@@ -1,52 +1,54 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 
 function Deckview() {
-const [decks, setDecks] = useState([]);
-let { id } = useParams();
-useEffect(() => {
-  const fetchDecks = () => {
-    const decklink = 'https://inazuma-tcg-api-879bee6c850b.herokuapp.com/decks/'+id
-    fetch(decklink, {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((responseData) => {
-        responseData.sort((a, b) => (a.cardid > b.cardid) ? 1 : -1)
-        console.log(responseData)
-        setDecks(responseData);
-      });
+    const [decks, setDecks] = useState([]);
+    let { id } = useParams();
+
+    const fetchDecks = () => {
+        const decklink = 'https://inazuma-tcg-api-879bee6c850b.herokuapp.com/decks/' + id;
+        fetch(decklink, {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((response) => response.json())
+        .then((responseData) => {
+            responseData.sort((a, b) => (a.cardid > b.cardid) ? 1 : -1);
+            setDecks(responseData);
+        });
     }
-    fetchDecks()
-}, []);
 
-function DelCard(idcard) {
-  const handleDelete = (idcard) => {
-    console.log("button pressed")
-    fetch('https://inazuma-tcg-api-879bee6c850b.herokuapp.com/decks/'+id+'/start/'+idcard, {method: 'DELETE'})
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+    useEffect(() => {
+        fetchDecks();
+    }, []);
 
-}}
+    const handleDelete = (idcard) => {
+        fetch('https://inazuma-tcg-api-879bee6c850b.herokuapp.com/decks/' + id + '/start/' + idcard, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            fetchDecks(); 
+        })
+        .catch(error => console.error(error));
+    }
 
-return (
-<div class="container columns8 bordered">
-{decks.map((data) => (
-    <div width="200px"  class="card-box card-display">
-      <div>
-        <img width="100%" src={data.picture} title={data.name} alt={data.name}></img>
-        <p>{data.name} {data.cardid}</p>
-        {<button onClick={(data.id).handleDelete}>delete</button>}
+    return (
+        <div className="container columns8 bordered">
+            {decks.map((data) => (
+                <div width="200px" className="card-box card-display" key={data.id}>
+                    <div>
+                        <img width="100%" src={data.picture} title={data.name} alt={data.name}></img>
+                        <p>{data.name} {data.cardid}</p>
+                        <button onClick={() => handleDelete(data.id)}>delete</button>
+                    </div>
+                </div>
+            ))}
         </div>
-    </div>
-  ))}
-</div>
-)
+    )
 }
 
-export default Deckview
+export default Deckview;
