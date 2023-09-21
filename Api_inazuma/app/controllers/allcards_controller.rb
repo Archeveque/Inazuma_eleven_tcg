a57d17381cbc4909b2df4213fff5e02acfcfa712
Archeveque <114493055+Archeveque@users.noreply.github.com>
2023-09-21 17:04:42 +0200

    added goalkeeper cards to index allcards_controller.rb

--- Api_inazuma/app/controllers/allcards_controller.rb
+++ Api_inazuma/app/controllers/allcards_controller.rb
@@ -1,6 +1,6 @@
 class AllcardsController < ApplicationController
     def index
-        @allcards= (StartingCard.all) +(ReserveCard.all) +(TechniqueCard.all)
+        @allcards= (StartingCard.all) +(ReserveCard.all) +(TechniqueCard.all)+(GoalCard.all)
         render json: @allcards
       end
       
