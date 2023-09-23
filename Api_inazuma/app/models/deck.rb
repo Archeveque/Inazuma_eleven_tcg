682c9f0d38d958f5e815acff3680a30b69ce9c27
Valentin Da silva <v.dasilva11d@gmail.com>
2023-09-23 14:52:33 +0200

    Add users to decks

--- Api_inazuma/app/models/deck.rb
+++ Api_inazuma/app/models/deck.rb
@@ -8,4 +8,7 @@ class Deck < ApplicationRecord
     has_many :starting_cards, through: :add_starting_to_decks
     has_many :reserve_cards, through: :add_reserve_to_decks
     has_many :technique_cards, through: :add_technique_to_decks
+
+    belongs_to :user
+
 end
--- Api_inazuma/app/models/user.rb
+++ Api_inazuma/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
-  has_many :articles
+  has_many :decks
 
   #  validates :email, presence: true
 end
