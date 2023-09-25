e1d156615855d2013fdc70f1832d0febcc39f063
DamiC0de <dreymidot@gmail.com>
2023-09-25 11:08:26 +0200

    redirect user after authentification

--- Api_inazuma/app/controllers/application_controller.rb
+++ Api_inazuma/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
     def after_sign_in_path_for(resource)
-        allcards_path
+        render json: { redirect_path: '/Catalog' }
     end
 end
