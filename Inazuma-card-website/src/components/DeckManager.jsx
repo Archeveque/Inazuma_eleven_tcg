f7a2f3cc23d55734be80ab4413f3b4016d6a16bc
DamiC0de <dreymidot@gmail.com>
2023-09-26 12:10:33 +0200

    remove in the deck on click "add to deck"

--- Inazuma-card-website/src/components/DeckManager.jsx
+++ Inazuma-card-website/src/components/DeckManager.jsx
@@ -228,9 +228,8 @@ function DeckManager() {
             <div className="card-box " key={"card" + data.cardid}>
               <img width="100%" src={data.picture} title={data.name} alt={data.name} onClick={() => handleImageClick(data.picture)} ></img>
       
-              {deck.some(deckCard => deckCard.cardid === data.cardid) ? 
-                <span>In the deck</span> : 
-                <button onClick={() => handleAddToDeck(data)}>Add to deck</button>}
+              <button onClick={() => handleAddToDeck(data)}>Add to deck</button>
+
       
             </div>
           ))}
